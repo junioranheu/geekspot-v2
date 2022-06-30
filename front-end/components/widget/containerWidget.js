c922f1284105aa97087be1945f522a55f9e0aa12
junioranheu <junioranheu@gmail.com>
2022-06-30 16:31:35 -0300

    criar função gerarOrdemTamanhosImagens() no componente \containerWidget

--- front-end/components/widget/containerWidget.js
+++ front-end/components/widget/containerWidget.js
@@ -1,12 +1,65 @@
 
 import Image from 'next/image';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import ImgCinza from '../../static/image/cinza.webp';
 import Styles from '../../styles/widget.module.css';
 
 export default function ContainerWidget({ titulo, descricao, listaWidgets }) {
     const tamanhoGrande = 366;
     const tamanhoPequeno = 176;
+
+    const [ordemTamanhosImagens, setOrdemTamanhosImagens] = useState([]);
+    useEffect(() => {
+        function gerarOrdemTamanhosImagens(qtd) {
+            let ordens = [];
+
+            for (let index = 0; index < qtd; index++) {
+                // Se o index for 0, isso significa que é o primeiro caso... vamos randomizar se o primeiro caso vai ser 1 [grande] ou 0 [pequeno];
+                if (index === 0) {
+                    // Se tiver apenas 1 item, automaticamente deve ser 1 [grande];
+                    if (qtd === 1) {
+                        ordens.push(1);
+                        continue;
+                    }
+
+                    // Randomizar 1 [grande] ou não;
+                    var randomBoolean = Math.random() < 0.5; // Aleatório, true ou false;
+                    ordens.push((randomBoolean ? 1 : 0));
+                    continue;
+                }
+
+                // Verificar se:
+                // #01 - O último é 1 [grande];
+                // #02 - Ou se o último é 0 [pequeno] e o penúltimo é 1 [grande];
+                // #03 - Se o index for 1 [o segundo da lista] e o último for 0 [pequeno];
+                // Caso sim... o próximo tem que ser 0 [pequeno];
+                const isOkParaSerPequeno =
+                    (ordens[index - 1] === 1) ||
+                    (ordens[index - 1] === 0 && ordens[index - 2] === 1) ||
+                    (index === 1 && ordens[index - 1] === 0);
+
+                if (isOkParaSerPequeno) {
+                    // Verificar se for o último item e o anterior for 1 [grande], esse AINDA deve ser 1 [grande]!
+                    const isDeveSeGrande = (index === qtd - 1) && (ordens[index - 1] === 1);
+                    // console.log(index === qtd, ordens[index - 1] === 1, isDeveSeGrande);
+
+                    if (isDeveSeGrande) {
+                        ordens.push(1); // Grande;
+                    } else {
+                        ordens.push(0); // Pequeno;
+                    }
+                } else {
+                    ordens.push(1); // Grande;
+                }
+            }
+
+            return ordens;
+        }
+
+        const ordens = gerarOrdemTamanhosImagens(listaWidgets.length);
+        // console.log(ordens);
+        setOrdemTamanhosImagens(ordens);
+    }, [listaWidgets.length]);
 
     return (
         <div className='flexColumn margem6'>
@@ -18,7 +71,7 @@ export default function ContainerWidget({ titulo, descricao, listaWidgets }) {
                     listaWidgets?.slice(0, 6).map((item, i) => (
                         <Fragment key={item.id}>
                             {
-                                item.tamanho === 2 ? (
+                                ordemTamanhosImagens[i] === 1 ? (
                                     // Tamanho grande
                                     <Image
                                         src={item.imagem}
@@ -29,7 +82,7 @@ export default function ContainerWidget({ titulo, descricao, listaWidgets }) {
                                     />
                                 ) : (
                                     // Tamanho pequeno;
-                                    listaWidgets[i + 1] && listaWidgets[i + 1]?.tamanho === 1 && (
+                                    listaWidgets[i + 1] && ordemTamanhosImagens[i + 1] === 0 && (
                                         <div className={Styles.divGrupoImagens}>
                                             <Image
                                                 src={item.imagem}
@@ -47,7 +100,7 @@ export default function ContainerWidget({ titulo, descricao, listaWidgets }) {
                                                 alt=''
                                             />
                                         </div>
-                                    ) 
+                                    )
                                 )
                             }
                         </Fragment>
