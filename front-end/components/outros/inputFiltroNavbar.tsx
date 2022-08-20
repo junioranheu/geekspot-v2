6861afdb84b92bbec899ac8a8576c7ad498b87d0
junioranheu <junioranheu@gmail.com>
2022-08-20 14:21:17 -0300

    typewriter-effect

--- front-end/components/outros/inputFiltroNavbar.tsx
+++ front-end/components/outros/inputFiltroNavbar.tsx
@@ -1,27 +1,46 @@
+import { Fragment, useEffect, useState } from 'react';
 import Typewriter from 'typewriter-effect'; // https://www.npmjs.com/package/typewriter-effect
 import Styles from '../../styles/navbar.padrao2.module.scss';
 import Lupa from '../svg/lupa';
 
 export default function InputFiltroNavbar() {
+
+    const teste = document.getElementsByClassName('Typewriter__wrapper');
+    const [efeitoTexto, setEfeitoTexto] = useState('');
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // @ts-ignore;
+            const typewriter = teste[0]?.innerText;
+            console.log(typewriter);
+            setEfeitoTexto(typewriter);
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [])
+
     return (
-        <div className={`${Styles.divLupa} animate__animated animate__fadeIn`}>
-            <div>
-                <input
-                    className={Styles.inputPesquisaNavbar}
-                    type='text'
-                    placeholder={`Busque algo aqui ${1}...`}
-                />
-
-                <Lupa width='1.5rem' cor='var(--preto)' />
-
+        <Fragment>
+            <div className='esconder'>
                 <Typewriter
                     options={{
-                        strings: ['Hello', 'World'],
+                        strings: ['"PS4"', '"Xbox"', '"Naruto"', '"iPhone"', '"Breaking bad"', '"Headset"', '"Guitarra"'],
                         autoStart: true,
-                        loop: true,
+                        loop: true
                     }}
                 />
             </div>
-        </div>
+
+            <div className={`${Styles.divLupa} animate__animated animate__fadeIn`}>
+                <div>
+                    <input
+                        className={Styles.inputPesquisaNavbar}
+                        type='text'
+                        placeholder={`Busque por ${efeitoTexto}`}
+                    />
+
+                    <Lupa width='1.5rem' cor='var(--preto)' />
+                </div>
+            </div>
+        </Fragment>
     )
 }
