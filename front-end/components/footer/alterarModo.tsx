ef89144d3cb70278de1305c91c976f0433cf8228
junioranheu <junioranheu@gmail.com>
2022-09-05 23:38:31 -0300

    alterarModo.tsx

---
+++ front-end/components/footer/alterarModo.tsx
@@ -0,0 +1,32 @@
+import { Dispatch } from 'react';
+import { StorageModoDark } from '../../utils/context/modoDarkContext';
+
+export default function alterarModo(isModoDark: boolean | undefined | null, setIsModoDark: Dispatch<any>) {
+    // console.log(isModoDark);
+
+    if (isModoDark === true) {
+        // console.log('Ativar modo dark'); 
+        document.documentElement.style.setProperty('--preto', '#FFFFFF'); // Preto fica branco;
+        document.documentElement.style.setProperty('--super-preto', '#f4f2f0'); // Super preto fica bege;
+        document.documentElement.style.setProperty('--branco', '#1A1A1A'); // Branco fica preto;
+        document.documentElement.style.setProperty('--cinza', '#F2F2F2'); // Cinza fica creme acinzentado;
+        document.documentElement.style.setProperty('--cinza-secundario', '#f4f2f0'); // Cinza escuro fica bege;  
+        document.documentElement.style.setProperty('--cor-border-hr', 'rgba(255, 255, 255, 10%)'); // Cinza "apagado" para branco "apagado";
+
+        // Atualizar no localStorage;
+        setIsModoDark(isModoDark);
+        StorageModoDark.update({ isModoDark: isModoDark });
+    } else if (isModoDark === false) {
+        // console.log('Ativar modo light');
+        document.documentElement.style.setProperty('--preto', '#1A1A1A');
+        document.documentElement.style.setProperty('--super-preto', '#000000');
+        document.documentElement.style.setProperty('--branco', '#FFFFFF');
+        document.documentElement.style.setProperty('--cinza', '#313131');
+        document.documentElement.style.setProperty('--cinza-secundario', '#242424');
+        document.documentElement.style.setProperty('--cor-border-hr', 'rgba(42, 42, 42, 10%)');
+
+        // Atualizar no localStorage;
+        setIsModoDark(isModoDark);
+        StorageModoDark.update({ isModoDark: isModoDark });
+    }
+}
--- front-end/components/footer/footer.tsx
+++ front-end/components/footer/footer.tsx
@@ -4,52 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import { useContext, useEffect } from 'react';
 import CONSTS_SISTEMA from '../../utils/consts/sistema';
-import { ModoDarkContext, StorageModoDark } from '../../utils/context/modoDarkContext';
+import { ModoDarkContext } from '../../utils/context/modoDarkContext';
+import alterarModo from './alterarModo';
 import Styles from './footer.module.scss';
 
 export default function Footer() {
     
     const modoDarkContext = useContext(ModoDarkContext); // Contexto do modo dark;
     const [isModoDark, setIsModoDark] = [modoDarkContext?.isModoDarkContext[0], modoDarkContext?.isModoDarkContext[1]];
-    const [teste, setTeste] = [modoDarkContext?.testeContext[0], modoDarkContext?.testeContext[1]];
 
     useEffect(() => {
-        alterarModo(isModoDark);
-    }, [isModoDark]);
-
-    useEffect(() => {
-        console.log('Teste {modoDarkContext?.testeContext[0]}', teste);
-    }, [teste]);
-
-    function alterarModo(isModoDark: boolean | undefined | null) {
-        // console.log(isModoDark);
- 
-        if (isModoDark === true) {
-            // console.log('Ativar modo dark'); 
-            document.documentElement.style.setProperty('--preto', '#FFFFFF'); // Preto fica branco;
-            document.documentElement.style.setProperty('--super-preto', '#f4f2f0'); // Super preto fica bege;
-            document.documentElement.style.setProperty('--branco', '#1A1A1A'); // Branco fica preto;
-            document.documentElement.style.setProperty('--cinza', '#F2F2F2'); // Cinza fica creme acinzentado;
-            document.documentElement.style.setProperty('--cinza-secundario', '#f4f2f0'); // Cinza escuro fica bege;  
-            document.documentElement.style.setProperty('--cor-border-hr', 'rgba(255, 255, 255, 10%)'); // Cinza "apagado" para branco "apagado";
-
-            // Atualizar no localStorage;
-            setIsModoDark(isModoDark);
-            StorageModoDark.update({ isModoDark: isModoDark });
-        } else if (isModoDark === false) {
-            // console.log('Ativar modo light');
-            document.documentElement.style.setProperty('--preto', '#1A1A1A');
-            document.documentElement.style.setProperty('--super-preto', '#000000');
-            document.documentElement.style.setProperty('--branco', '#FFFFFF');
-            document.documentElement.style.setProperty('--cinza', '#313131');
-            document.documentElement.style.setProperty('--cinza-secundario', '#242424');
-            document.documentElement.style.setProperty('--cor-border-hr', 'rgba(42, 42, 42, 10%)');
-
-            // Atualizar no localStorage;
-            setIsModoDark(isModoDark);
-            StorageModoDark.update({ isModoDark: isModoDark });
-        }
-    }
+        alterarModo(isModoDark, setIsModoDark);
+    }, [isModoDark, setIsModoDark]);
 
     return (
         <footer className={Styles.footer}>
@@ -122,7 +88,7 @@ export default function Footer() {
                 <div className={Styles.direita}>
                     <div className={Styles.icones}>
                         <span title={`${(isModoDark ? 'Alternar para o modo light' : 'Alternar para o modo dark')}`}>
-                            <FontAwesomeIcon className='pointer cor-principal-hover' icon={(isModoDark ? faSun : faMoon)} size='lg' onClick={() => alterarModo(!isModoDark)} />
+                            <FontAwesomeIcon className='pointer cor-principal-hover' icon={(isModoDark ? faSun : faMoon)} size='lg' onClick={() => alterarModo(!isModoDark, setIsModoDark)} />
                         </span>
 
                         <span title='GitHub'>
