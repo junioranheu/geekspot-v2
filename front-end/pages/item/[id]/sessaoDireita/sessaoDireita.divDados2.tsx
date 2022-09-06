c332c433e12e8be25b75eea7be848e8d262632cb
junioranheu <junioranheu@gmail.com>
2022-09-05 21:25:06 -0300

    <divDados2/>

---
+++ front-end/pages/item/[id]/sessaoDireita/sessaoDireita.divDados2.tsx
@@ -0,0 +1,34 @@
+import Styles from './index.module.scss';
+
+export default function divDados2({ item }: any) {
+    return (
+        <div className={`${Styles.divDados2} margem1_5 flexColumn`}>
+            <div className='flexColumn'>
+                <span>{(item.descricao ?? '-')}</span>
+            </div>
+
+            <span className={Styles.separadorHorizontal}></span>
+
+            <div className={Styles.divDados2Interna}>
+                <div className='flexColumn'>
+                    <span>Marca</span>
+                    <span>{(item.marca ?? '-')}</span>
+                </div>
+
+                <span className='separador'></span>
+
+                <div className='flexColumn'>
+                    <span>Condição</span>
+                    <span>{(item.condicao ?? '-')}</span>
+                </div>
+
+                <span className='separador'></span>
+
+                <div className='flexColumn'>
+                    <span>Código</span>
+                    <span>{(item.itemId ? `#${item.itemId}` : '-')}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
