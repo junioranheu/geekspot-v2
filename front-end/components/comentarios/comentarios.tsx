a09f056951774f48be9302f29f58b2f4343ba26e
junioranheu <junioranheu@gmail.com>
2022-09-08 21:49:00 -0300

    Comentarios

--- front-end/components/comentarios/comentarios.tsx
+++ front-end/components/comentarios/comentarios.tsx
@@ -3,7 +3,8 @@ import Styles from './comentarios.module.scss';
 export default function Comentarios() {
     return (
         <div className={Styles.main}>
-            <textarea className={Styles.input} placeholder='Pergunte ao vendedor'></textarea>
+            <textarea className='textarea' placeholder='Pergunte ao vendedor'>
+            </textarea>
 
             <h1>teste</h1>
         </div>
