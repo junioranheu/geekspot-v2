9f61f5b07cbcbb3945bb3a1ee37df3d0e6ff9bee
junioranheu <junioranheu@gmail.com>
2022-09-14 13:28:07 -0300

    melhorias em verificarTokenValido.ts

--- front-end/utils/outros/converterTempoDecimalEmFormatoPadrao.ts
+++ front-end/utils/outros/converterTempoDecimalEmFormatoPadrao.ts
@@ -2,11 +2,16 @@ import moment from 'moment';
 
 // https://stackoverflow.com/a/44071876;
 export default function converterTempoDecimalEmFormatoPadrao(tempo: number, tipoDuracao: any) {
-    const d = moment.duration(tempo, tipoDuracao);
+    const data = moment.duration(tempo, tipoDuracao);
 
-    return [
-        ('0' + d.hours()).slice(-2),
-        ('0' + d.minutes()).slice(-2),
-        ('0' + d.seconds()).slice(-2),
-    ].join(':')
+    const d = data.asDays() >= 1 ? (`${data.asDays().toFixed(0)} ${data.asDays().toFixed(0) === '1' ? 'dia' : 'dias'}, `) : '';
+
+    const hms = [
+        ('0' + data.hours()).slice(-2),
+        ('0' + data.minutes()).slice(-2),
+        ('0' + data.seconds()).slice(-2),
+    ].join(':');
+
+    const resultadoFinal = d + hms;
+    return resultadoFinal;
 }
