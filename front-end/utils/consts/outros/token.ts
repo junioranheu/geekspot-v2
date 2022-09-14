dabaf46a92e01946751bb617ecb762463d1b2168
junioranheu <junioranheu@gmail.com>
2022-09-14 11:03:48 -0300

    melhorar função verificarTokenValido.ts

--- front-end/utils/consts/outros/token.ts
+++
@@ -1,5 +0,0 @@
-const TOKEN = {
-   LIMITE_EXPIRAR_TOKEN_HORAS: 12 // Mesmo valor do appsettings.json no back-end (JwtSettings);
-};
-
-export default TOKEN;
--- front-end/utils/outros/verificarTokenValido.ts
+++ front-end/utils/outros/verificarTokenValido.ts
@@ -1,7 +1,7 @@
+import moment from 'moment';
 import Router from 'next/router';
 import nProgress from 'nprogress';
 import CONSTS_SISTEMA from '../consts/outros/sistema';
-import TOKEN from '../consts/outros/token';
 import { Auth } from '../context/usuarioContext';
 import { Aviso } from './aviso';
 import diferencaDatasEmHoras from './diferencaDatasEmHoras';
@@ -9,15 +9,25 @@ import horarioBrasilia from './horarioBrasilia';
 
 export default function verificarTokenValido(isAuth: boolean | undefined, setIsAuth: any) {
     if (isAuth) {
-        const horaAgora = horarioBrasilia().format('YYYY-MM-DD HH:mm:ss');
-        const dataAutenticacao = Auth.get()?.dataAutenticacao;
-        var diferencaHoras = diferencaDatasEmHoras(horaAgora, dataAutenticacao);
+        const token = Auth?.get()?.token ?? '';
+        const dataExpiracaoToken = moment(getJWTExpireDate(token));
+        const horaAgora = moment(horarioBrasilia());
 
-        // console.log('horaAgora:', horaAgora);
-        // console.log('dataAutenticacao:', dataAutenticacao);
-        // console.log('diferencaHoras:', diferencaHoras);
+        if (process.env.NODE_ENV === 'development') {
+            try {
+                const info = {
+                    'Data atual': horaAgora.format('YYYY-MM-DD HH:mm:ss'),
+                    'Data de expiração do token': dataExpiracaoToken.format('YYYY-MM-DD HH:mm:ss'),
+                    'Horas faltantes': diferencaDatasEmHoras(dataExpiracaoToken, horaAgora)
+                }
 
-        if (diferencaHoras >= TOKEN.LIMITE_EXPIRAR_TOKEN_HORAS) {
+                console.table(info);
+            } catch (error) {
+
+            }
+        }
+
+        if (horaAgora > dataExpiracaoToken) {
             nProgress.start();
             Aviso.custom(`A sua sessão expirou!<br/><br/>Renove sua sessão fazendo login novamente no ${CONSTS_SISTEMA.NOME_SISTEMA} 😎`, 15000);
 
@@ -30,4 +40,22 @@ export default function verificarTokenValido(isAuth: boolean | undefined, setIsA
             nProgress.done();
         }
     }
+}
+
+// https://stackoverflow.com/a/67802771
+function getJWTExpireDate(jwtToken: string) {
+    if (jwtToken) {
+        try {
+            const [, payload] = jwtToken.split('.');
+            const { exp: expires } = JSON.parse(window.atob(payload));
+
+            if (typeof expires === 'number') {
+                return new Date(expires * 1000);
+            }
+        } catch {
+            // ignore
+        }
+    }
+
+    return null;
 }
