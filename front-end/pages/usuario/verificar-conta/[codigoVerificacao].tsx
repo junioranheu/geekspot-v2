d695ec3929d1020b06a79755a10b7bfd7a89829f
junioranheu <junioranheu@gmail.com>
2022-09-20 00:12:12 -0300

    iniciar desenvolvimento da tela \usuario\verificar-conta\[codigoVerificacao].tsx

---
+++ front-end/pages/usuario/verificar-conta/[codigoVerificacao].tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import CONSTS_SISTEMA from '../../../utils/consts/outros/sistema';
+import paginaCarregada from '../../../utils/outros/paginaCarregada';
+
+export default function VerificarConta() {
+    document.title = `Verificar conta — ${CONSTS_SISTEMA.NOME_SISTEMA}`;
+
+    const router = useRouter()
+    const { codigoVerificacao } = router.query
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        // const url = `${CONSTS_USUARIOS.API_URL_GET_BY_ID}/${id}`;
+        // const usuario = await Fetch.getApi(url, null) as iUsuario;
+
+        console.log(codigoVerificacao);
+        paginaCarregada(true, 200, 500, setIsLoaded);
+    }, [codigoVerificacao]);
+
+    if (!isLoaded) {
+        return false;
+    }
+
+    return (
+        <section className='flexColumn paddingPadrao margem3'>
+            <div className='centralizarTexto'>
+                <h1>aaaaaaaaaaaea</h1>
+            </div>
+
+            <div className='margem2'>
+                Teste
+            </div>
+        </section>
+    )
+}
