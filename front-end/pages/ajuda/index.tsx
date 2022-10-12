1a6b9aca961541c950c7b757db50c440c5e2164f
junioranheu <junioranheu@gmail.com>
2022-10-11 23:29:12 -0300

    iniciar desenvolvimento da tela Ajuda

--- front-end/pages/ajuda/index.tsx
+++ front-end/pages/ajuda/index.tsx
@@ -1,13 +1,28 @@
+import Image from 'next/image';
+import EmojiMedicacao from '../../static/image/outros/emoji-meditacao.webp';
 import CONSTS_SISTEMA from '../../utils/consts/outros/sistema';
 import Styles from './index.module.scss';
+import InputPesquisaTopicoAjuda from './outros/inputPesquisaTopicoAjuda';
 
 export default function Index() {
     document.title = `Ajuda — ${CONSTS_SISTEMA.NOME_SISTEMA}`;
 
     return (
-        <section className={Styles.main}>
-            <h1>Ajuda</h1>
+        <section className={`${Styles.main} paddingPadrao paddingPadraoMargemGrande`}>
+            {/* #1 - Título */}
+            <div className={Styles.divTitulo}>
+                <span>Central de ajuda</span>
+
+                <div className='animate__animated animate__pulse animate__slower animate__infinite'>
+                    <Image src={EmojiMedicacao} alt='' width={50} height={63} />
+                </div>
+            </div>
+
+            {/* #2 - Input para filtragem dos tópicos */}
+            <InputPesquisaTopicoAjuda />
+
+            {/* #3 - Tópicos */}
         </section>
     )
 }
- 
+
---
+++ front-end/pages/ajuda/outros/inputPesquisaTopicoAjuda.tsx
@@ -0,0 +1,34 @@
+import { KeyboardEvent, useState } from 'react';
+import Lupa from '../../../components/svg/lupa';
+import Styles from '../index.module.scss';
+
+export default function InputPesquisaTopicoAjuda() {
+
+    function handleKeyPress(e: KeyboardEvent<HTMLInputElement>) {
+        if (e.key === 'Enter') {
+            handleBuscar();
+        }
+    }
+
+    const [txtFiltro, setTxtFiltro] = useState('');
+    function handleBuscar() {
+        console.log('handleBuscar():', txtFiltro);
+    }
+
+    return (
+        <div className={`${Styles.divPesquisa} margem3`}>
+            <input
+                className={Styles.inputPesquisaNavbar}
+                type='text'
+                placeholder='Procure por um tópico'
+                onChange={(e) => setTxtFiltro(e.target.value)}
+                onKeyPress={handleKeyPress}
+            />
+
+            <div className={Styles.lupa} title='Buscar tópico' onClick={() => handleBuscar()}>
+                <Lupa width={20} url={null} title={null} isCorPrincipal={false} />
+            </div>
+        </div>
+    )
+}
+
