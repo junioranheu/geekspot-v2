2780080bf3a674e6dc36f726dab19f577bde29c3
junioranheu <junioranheu@gmail.com>
2022-11-25 18:23:00 -0300

    adicionar método FormatarBytes() à Biblioteca.s

--- back-end/GeekSpot.Utils/Biblioteca.cs
+++ back-end/GeekSpot.Utils/Biblioteca.cs
@@ -144,6 +144,21 @@ namespace GeekSpot.Utils
             return formFiles[0];
         }
 
+        // Formatar bytes para B, KB, MB, etc...;
+        public static string FormatarBytes(long bytes)
+        {
+            string[] Suffix = { "B", "KB", "MB", "GB", "TB" };
+            int i;
+            double dblSByte = bytes;
+
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+
+            return String.Format("{0:0.##} {1}", dblSByte, Suffix[i]);
+        }
+
         // Validar se o e-mail do usuário;
         public static bool ValidarEmail(string email)
         {
